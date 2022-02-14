29ce6ce0187336cfe20b105117c6ab15fbfb52ac
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-02-14 21:27:29 +0400

    Исправления ошибок в золотом сечении

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,7 +6,12 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float x0 = -3f;
+            float x1 = 3f;
+            float eps = 0.2f;
+            float ans1 = dichotomy(parabole, x0, x1, eps);
+            float ans2 = golden(parabole, x0, x1, eps);
+            Console.WriteLine($"Dichotomy: {ans1} \nGolden: {ans2}");
         }
 
         static float dichotomy(Func<float, float> f, float x0, float x1, float eps)
@@ -14,27 +19,31 @@ namespace Lab1
             float y1, y2;
             while (true)
             {
-                y1 = f((x0 + x1) / 2 - eps);
-                y2 = f((x0 + x1) / 2 + eps);
-                if (y1 < y2) x0 = (x0 + x1) / 2 - eps;
-                else x0 = (x0 + x1) / 2 + eps;
-                if (Math.Abs(x1 - x0) < 2 * eps) return (x1 + x0) / 2;
+                y1 = f((x0 + x1) * 0.5f - eps);
+                y2 = f((x0 + x1) * 0.5f + eps);
+                if (y1 < y2) x1 = (x0 + x1) * 0.5f - eps;
+                else x0 = (x0 + x1) * 0.5f + eps;
+                if (Math.Abs(x1 - x0) < 2 * eps) return (x1 + x0) * 0.5f;
             }
         }
 
         static float golden(Func<float, float> f, float x0, float x1, float eps)
         {
             float y1, y2;
-            float fi1 = (float)(2 / (1 + Math.Sqrt(5)));
-            float fi2 = (float)((-1 + Math.Sqrt(5)) / (1 + Math.Sqrt(5));
+            float fi1 = (float)((1 + Math.Sqrt(5)) * 0.5f);
             while (true)
             {
-                y1 = f((x0 + x1) / fi1 - eps);
-                y2 = f((x0 + x1) / fi2 + eps);
-                if (y1 < y2) x0 = (x0 + x1) / 2 - eps;
-                else x0 = (x0 + x1) / 2 + eps;
-                if (Math.Abs(x1 - x0) < 2 * eps) return (x1 + x0) / 2;
+                y1 = f(x0 + (x1 - x0) / fi1);
+                y2 = f(x1 - (x1 - x0) / fi1);
+                if (y1 < y2) x0 = x1 - (x1 - x0) / fi1;
+                else x1 = x0 + (x1 - x0) / fi1;
+                if (Math.Abs(x1 - x0) < 2 * eps) return (x1 + x0) * 0.5f;
             }
+        }
+
+        static float parabole(float x)
+        {
+            return (float)Math.Pow(x, 2.0);
         }
     }
 }
