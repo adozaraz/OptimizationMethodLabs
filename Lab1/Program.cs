b058be47ae3d8e70c55b90d4d889ef7d8eae4145
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-02-14 22:09:44 +0400

    Сомнительное решение методом фибоначчи

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -9,10 +9,12 @@ namespace Lab1
             float x0 = -3f;
             float x1 = 3f;
             float eps = 0.2f;
+            float delta = 0.1f;
             Lab1 f = new Lab1();
             float ans1 = f.dichotomy(f.parabole, x0, x1, eps);
             float ans2 = f.golden(f.parabole, x0, x1, eps);
-            Console.WriteLine($"Dichotomy: {ans1} \nGolden: {ans2}");
+            float ans3 = f.fibonacciMethod(f.parabole, x0, x1, delta, eps);
+            Console.WriteLine($"Dichotomy: {ans1} \nGolden: {ans2}\nFibonacci: {ans3}");
         }
 
         
--- Lab1/lab1.cs
+++ Lab1/lab1.cs
@@ -27,10 +27,42 @@ namespace Lab1
             {
                 y1 = f(x0 + (x1 - x0) / fi1);
                 y2 = f(x1 - (x1 - x0) / fi1);
-                if (y1 < y2) x0 = x1 - (x1 - x0) / fi1;
-                else x1 = x0 + (x1 - x0) / fi1;
+                if (y1 > y2) x1 = x0 + (x1 - x0) / fi1;
+                else x0 = x1 - (x1 - x0) / fi1;
                 if (Math.Abs(x1 - x0) < 2 * eps) return (x1 + x0) * 0.5f;
             }
+        }
+
+        public float fibonacciMethod(Func<float, float> f, float x0, float x1, float delta, float eps)
+        {
+            float y1, y2;
+            float fib1 = 2, fib2 = 1;
+            static int findN(float a, float b, float delta)
+            {
+                if ((a + b) / delta <= 1) return 2;
+                else if ((a + b) / delta <= 2) return 3;
+                else
+                {
+                    int fib1 = 2, fib2 = 1;
+                    int n = 3;
+                    while (true)
+                    {
+                        fib1 += fib2;
+                        fib2 = fib1 - fib2;
+                        ++n;
+                        if (fib1 >= (a + b) / delta) return n;
+                    }
+                }
+            }
+            int n = findN(x0, x1, delta);
+            for (int i = 0; i < n; ++i)
+            {
+                y1 = f((x0 + x1) * (fib2) / fib1 + eps);
+                y2 = f((x0 + x1) * (fib1 - fib2) / fib1 + eps);
+                if (y1 > y2) x1 = (x0 + x1) * (fib1 - fib2) / fib1 + eps;
+                else x0 = (x0 + x1) * (fib1 - fib2) / fib1 + eps;
+            }
+            return (x1 - x0) * 0.5f;
         }
 
         public float parabole(float x)
