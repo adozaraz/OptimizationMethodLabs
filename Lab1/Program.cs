471d5df8258ebf0b1e6172a198bf752a068ab2eb
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-02-28 15:03:32 +0400

    Попытка сделать 2 лабу

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Lab1
 {
@@ -10,13 +11,16 @@ namespace Lab1
             float x1 = 3f;
             float eps = 0.2f;
             float delta = 0.1f;
-            Lab1 f = new Lab1();
+            float lambda = 2;
+            Vector2 vec2 = new Vector2(3, 3);
+            /*
             float ans1 = f.dichotomy(f.parabole, x0, x1, eps);
             float ans2 = f.golden(f.parabole, x0, x1, eps);
             float ans3 = f.fibonacciMethod(f.parabole, x0, x1, delta, eps);
-            Console.WriteLine($"Dichotomy: {ans1} \nGolden: {ans2}\nFibonacci: {ans3}");
+            Console.WriteLine($"Dichotomy: {ans1} \nGolden: {ans2}\nFibonacci: {ans3}");*/
+            Lab2 lab2 = new Lab2();
+            Vector2 ans = lab2.per_coordinate_descend(Lab1.stoneParabole, vec2, eps, lambda);
+            Console.WriteLine($"Descent: ({ans.X}, {ans.Y})");
         }
-
-        
     }
 }
--- Lab1/lab1.cs
+++ Lab1/lab1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Numerics;
 
 namespace Lab1
 {
     class Lab1
     {
-        public float dichotomy(Func<float, float> f, float x0, float x1, float eps)
+        public static float dichotomy(Func<float, float> f, float x0, float x1, float eps)
         {
             float y1, y2;
             while (true)
@@ -19,7 +20,29 @@ namespace Lab1
             }
         }
 
-        public float golden(Func<float, float> f, float x0, float x1, float eps)
+        public static float diochotomyModified(Func<float, float, float> f, Vector2 x0, Vector2 x1, float eps)
+        {
+            int i = 0;
+            float z1, z2;
+            while (true)
+            {
+                z1 = f((x0.X + x1.X) * 0.5f - eps, (x0.Y + x1.Y) * 0.5f - eps);
+                z2 = f((x0.X + x1.X) * 0.5f + eps, (x0.Y + x1.Y) * 0.5f + eps);
+                if (z1 < z2)
+                {
+                    if (x1.X == x0.X) x1.Y = (x0.Y + x1.Y) * 0.5f - eps;
+                    else x1.X = (x0.X + x1.X) * 0.5f - eps;
+                }
+                else
+                {
+                    if (x0.X == x1.X) x0.Y = (x0.Y + x1.Y) * 0.5f + eps;
+                    else x0.X = (x0.X + x1.X) * 0.5f + eps;
+                }
+                if (Vector2.Dot(x0, x1) < 2 * eps || ++i > 999) return Vector2.Dot(x0, x1) * 0.5f;
+            }
+        }
+
+        public static float golden(Func<float, float> f, float x0, float x1, float eps)
         {
             float y1, y2;
             float fi1 = (float)((1 + Math.Sqrt(5)) * 0.5f);
@@ -33,7 +56,7 @@ namespace Lab1
             }
         }
 
-        public float fibonacciMethod(Func<float, float> f, float x0, float x1, float delta, float eps)
+        public static float fibonacciMethod(Func<float, float> f, float x0, float x1, float delta, float eps)
         {
             float y1, y2;
             float fib1 = 2, fib2 = 1;
@@ -65,9 +88,14 @@ namespace Lab1
             return (x1 - x0) * 0.5f;
         }
 
-        public float parabole(float x)
+        public static float parabole(float x)
         {
             return (float)Math.Pow(x, 2.0);
+        }
+
+        public static float stoneParabole(float x, float y)
+        {
+            return (float)Math.Pow(x, 2) + (float)Math.Pow(y, 2);
         }
     }
 }
